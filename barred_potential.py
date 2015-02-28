72acd7d8a330cd2dc3e2ae189bb31f9d188d9a07
Alexa Villaume <avillaum@ucsc.edu>
2015-02-27 16:36:55 -0800

    I think I'm getting the correct orbits now

--- barred_potential.py
+++ barred_potential.py
@@ -10,7 +10,7 @@ import bsint
 
 num_bodies = 1      # See pythagorean_three_body.py
 t0 = 0
-t1 = 10e0            # endpoints for the time array
+t1 = 10e2            # endpoints for the time array
 ecce = np.sqrt(0.01)
 R_c = 0.5           # don't know what this represents
 V0_sqr = 1    # don't know what this represents
@@ -39,7 +39,7 @@ def compute_vy(x_init, label):
 
     init_conds = create_initial_values(x_init)
     if label == 'Outer Linblad':
-        init_conds[3] = init_conds[0]*np.sqrt(V0_sqr/(R_c**2 + init_conds[0]**2 )) - init_conds[0]*Omega_b
+        init_conds[3] = init_conds[0]*np.sqrt(V0_sqr/(R_c**2 + init_conds[0]**2)) - init_conds[0]*Omega_b
 
     return init_conds
 
@@ -53,13 +53,9 @@ def derivs(t, init_conds):
     r_b = init_conds[0]**2 - init_conds[2]**2
     term1 = R_c**2 + r_sqr
 
-    print init_conds
-    coriolis = np.cross([init_conds[1], init_conds[3], 0], [0, 0, Omega_b])
+    coriolis = 2*np.cross([init_conds[1], init_conds[3], 0], [0, 0, Omega_b])
     centrifugal = np.cross(np.cross([0, 0, Omega_b], [init_conds[0], init_conds[2], 0]), [0, 0, Omega_b])
 
-
-    print coriolis, centrifugal
-    #sys.exit()
     x_acc = -(V0_sqr*init_conds[0]*(ecce**2 * (r_b/r_sqr) + 1)/term1) + \
             (0.5*V0_sqr)*(((2*(ecce**2)*init_conds[0])/r_sqr) - \
             (2*(ecce**2)*init_conds[0]*r_b)/r_sqr**2)*np.log(term1) + \
@@ -74,12 +70,13 @@ def derivs(t, init_conds):
 
 def integrate(x_initial, ax, label):
     init_conds = compute_vy(x_initial, label)
-    out, tout = bsint.bsintegrate(derivs, init_conds, t0, t1, tacc=1e-14, mxstep=20000)
-    ax.plot(out[:,0], out[:,2], color='#424242', alpha=0.5)
-    ax.annotate(label, xy=(1,0), xycoords='axes fraction', xytext=(0.95, 0.95),
-                ha='right', va='top', fontsize=12)
-    ax.set_xlabel('x-position', fontsize=16)
-    ax.set_ylabel('y-position', fontsize=16)
+    print label, init_conds
+    #out, tout = bsint.bsintegrate(derivs, init_conds, t0, t1, tacc=1e-14, mxstep=20000)
+    #ax.plot(out[:,0], out[:,2], color='#424242', alpha=0.5)
+    #ax.annotate(label, xy=(1,0), xycoords='axes fraction', xytext=(0.95, 0.95),
+    #            ha='right', va='top', fontsize=12)
+    #ax.set_xlabel('x-position', fontsize=16)
+    #ax.set_ylabel('y-position', fontsize=16)
 
 if __name__ == '__main__':
     fig = plt.figure(figsize=(18,8))
